7864e55b344ae154a88dd0c6348f73b26b19c46f
Amber Woolam <akw31108@email.vccs.edu>
2025-02-20 14:15:04 -0800

    troubleshot the errors I was running into and fixed everything

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 This is a local deployment to serve the GitHub Pages app of [akw31108.github.io/client-side-demos]. (https://github.io/client-side-demos)
 
 ## Architecture
-TBD
+-requests to `http://localhost:8081` get routed to the `fp-svc` which has a webserver on port `7901`
+    -for `http://fp-svc:7901/`, the container proxies to `http://hp-svc:6969/`
+    -for `http://fp-svc:7901/pages-site`, the container serves the pages site stored inside the image at `/usr/share/nginx/html`
+    - the `hp-svc` serves a landing page on port `6969` that comes from a volume and has a link to `http://localhost:8081/pages-site/`
 
 ## Prerequisites
 - Docker version 27.4.0, build bde2b89
--- scripts/init.sh
+++ scripts/init.sh
@@ -3,10 +3,10 @@
 mkdir -p volumes/final-project/config
 rm -rf volumes/final-project/config/*
 mkdir -p volumes/home-page/{config,html}
-rm -rf volumes/home-page/{config,html}
+rm -rf volumes/home-page/{config,html}/*
 
 docker pull nginx:alpine3.21
-docker run --rm --name temp-nginx -d nginx:alpine3.21
+docker run --rm --name temp-nginx -d nginx:alpine3.21 
 
 docker cp temp-nginx:/etc/nginx/conf.d volumes/final-project/config
 docker cp temp-nginx:/etc/nginx/nginx.conf volumes/final-project/config/nginx.conf
@@ -20,21 +20,19 @@ docker stop temp-nginx
 
 sed -i 's/80/7901/g' volumes/final-project/config/conf.d/default.conf
 
-LOC_BLOCK$(cat <<EOF
-    location / {
-        root    /usr/share/nginx/html;
-        index   index.html index.htm;
-    }
+LOC_BLOCK=$(cat <<EOF
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
 EOF
 )
-FP_REPO_NAME="pages-site"
+FP_REPO_NAME="itp-docker"
 NEW_LOC_BLOCK=$(cat <<EOF
     location / {
         proxy_pass http://hp-svc:6969;
     }
     location /$FP_REPO_NAME {
         alias   /usr/share/nginx/html;
-        index   index.html index.htm;
+        index index.html index.htm;
     }
 EOF
 )
@@ -42,10 +40,9 @@ EOF
 perl -0777 -i -pe "s#$LOC_BLOCK#$NEW_LOC_BLOCK#g" volumes/final-project/config/conf.d/default.conf
 
 sed -i 's/80/6969/g' volumes/home-page/config/conf.d/default.conf
-
 OLD_HTML_BODY=$(cat <<EOF
 <body>
-<h1>Welcome to nginx:</h1>
+<h1>Welcome to nginx!</h1>
 <p>If you see this page, the nginx web server is successfully installed and
 working. Further configuration is required.</p>
 
@@ -61,7 +58,7 @@ EOF
 
 NEW_HTML_BODY=$(cat <<EOF
 <body>
-<h1>Home</h1>
+<h1>Home</h1
     <p>Please visit the <a href="/$FP_REPO_NAME/">$FP_REPO_NAME</a> page.</p>
 </body>
 EOF
